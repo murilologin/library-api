f680489d71e43e5c5c3ee985c7686724c56adad2
Jorge Murilo <murilo.login@gmail.com>
2023-04-21 16:42:08 -0300

    final do curso

--- src/main/java/com/cursodsousa/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/cursodsousa/libraryapi/LibraryApiApplication.java
@@ -1,21 +1,16 @@
 package com.cursodsousa.libraryapi;
 
-import com.cursodsousa.libraryapi.service.EmailService;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
-import java.util.Arrays;
-import java.util.List;
-
 @SpringBootApplication
 @EnableScheduling
 public class LibraryApiApplication {
+
+	//usar public class LibraryApiApplication extends SpringBootServletInitializer { para tomcat, alterar no pom também
 
 	//@Autowired
 	//private EmailService emailService;
